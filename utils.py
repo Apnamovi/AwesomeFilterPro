5e679e190fe7d66c79d329faf950a795a917d62a
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-03-09 23:13:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -366,7 +366,6 @@ def remove_escapes(text: str) -> str:
             res += text[counter]
     return res
 
-
 def humanbytes(size):
     if not size:
         return ""
@@ -378,3 +377,26 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://shorturllink.in/api'
+    params = {'api': SHORTNER_API,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
